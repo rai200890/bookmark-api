3c5cd8171b7776b370727a2dfcb2b0e9f5e60528
Raissa Ferreira <rai200890@gmail.com>
2016-10-03 19:33:26 -0300

    Add CORS support with flask-cors
    - Add missing database migration

--- bookmark_api/__init__.py
+++ bookmark_api/__init__.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
+from flask_cors import CORS, cross_origin
 
 
 dotenv_path = join(dirname(__file__), '.env')
@@ -21,6 +22,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = parse_boolean(environ.get('SQLALCHEMY_TRACK_MODIFICATIONS'))
 app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
 app.config['JWT_VERIFY'] = parse_boolean(environ.get('JWS_VERIFY'))
+app.config['CORS_ORIGINS'] = environ.get('CORS_ORIGINS')
+
 
 db = SQLAlchemy(app)
 api = Api(app)
+CORS(app)
