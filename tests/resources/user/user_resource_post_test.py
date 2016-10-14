fb315ab25e79b0112f2e691ea44acb4fc89f6351
Raissa Ferreira <rai200890@gmail.com>
2016-10-14 20:12:16 -0300

    Remove required permission from user creation

--- tests/resources/user/user_resource_post_test.py
+++ tests/resources/user/user_resource_post_test.py
@@ -15,6 +15,18 @@ def create_valid_params():
 
 
 @pytest.fixture
+def create_valid_params_admin(admin_role):
+    return {
+        "user": {
+            "username": "john_doe",
+            "email": "john_doe@email.com",
+            "password": "unknown",
+            "role_id": admin_role.id
+        }
+    }
+
+
+@pytest.fixture
 def create_invalid_params():
     return {
         "user": {
@@ -28,6 +40,15 @@ def test_post_valid(api_test_client, create_valid_params):
     response = api_test_client.post('/users',
                                     data=json.dumps(create_valid_params),
                                     headers={"content-type": "application/json"})
+    data = json.loads(response.data.decode('utf-8'))
+    assert response.status_code == 200
+    assert data['user']
+
+
+def test_post_valid_admin(api_test_client, create_valid_params_admin, admin_auth_headers):
+    response = api_test_client.post('/users',
+                                    data=json.dumps(create_valid_params_admin),
+                                    headers=admin_auth_headers)
     data = json.loads(response.data.decode('utf-8'))
     assert response.status_code == 200
     assert data['user']
